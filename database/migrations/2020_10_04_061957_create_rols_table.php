58b404154e774d87b5bc7a586d382601835e3ca1
Neibeth Chango <neiichango@gmail.com>
2020-10-05 20:54:45 -0600

    corrected DB suggestion non working yet

--- database/migrations/2020_10_04_061957_create_rols_table.php
+++ database/migrations/2020_10_04_061957_create_rols_table.php
@@ -18,6 +18,7 @@ class CreateRolsTable extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->timestamps();
+            
         });
     }
 
