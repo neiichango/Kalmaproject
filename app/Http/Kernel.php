dacf1e5564d2d2f0cb5e1228d6c15f7a3217431e
Neibeth Chango <neiichango@gmail.com>
2020-10-11 19:24:58 -0600

    passport

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\ForceJsonResponse::class,
     ];
 
     /**
@@ -62,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'json.reponse'=> \App\Http\Middleware\ForceJsonResponse::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
     ];
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
+
     ];
 
     /**
@@ -24,7 +26,19 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes();
 
-        //
+        //definir el alcance
+
+        Passport::tokensCan([
+            'Administrador' => 'Mantenimiento de productos personal de envio,ver estado de los pedidos',
+            'Vendedor' => 'Mantenimiento de pedidos',
+            'Bodeguero' => 'Mantenimiento de pedido, encargado del estado del empaque',
+            'Entrega' => 'Mantenimiento de pedidos,encargado del estado de entrega',
+        ]);
+
+        Passport::setDefaultScope([
+            'Vendedor'
+        ]);
     }
 }
